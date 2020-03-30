99a4c2f582204c24e1facc553a31722a10e0f4fc
BEAST0811 <51814309+BEAST0811@users.noreply.github.com>
2020-03-31 00:07:10 +0530

    Add files via upload

---
+++ rename.py
@@ -0,0 +1,14 @@
+import os
+
+imdir = 'images'
+if not os.path.isdir(imdir):
+    os.mkdir(imdir)
+
+fidget_folders = [folder for folder in os.listdir('.') if 'carDataset' in folder]
+print(os.listdir('.'))
+
+n = 0
+for folder in fidget_folders:
+    for imfile in os.scandir(folder):
+        os.rename(imfile.path, os.path.join(imdir, '{:06}.jpg'.format(n)))
+        n += 1
